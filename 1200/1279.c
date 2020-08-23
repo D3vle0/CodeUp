5e97ad69bdbc08c80cb5e209a22a0cc18ed770a9
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2020-08-23 02:09:43 -0400

    0823

--- 1200/1279.c
+++ 1200/1279.c
@@ -2,8 +2,7 @@
 int main() {
     int a,b,sum=0;
     scanf("%d %d", &a, &b);
-    sum=a;
-    for (int i=a+1;i<=b;i++){
+    for (int i=a;i<=b;i++){
         if (i%2==0)
             sum-=i;
         else
